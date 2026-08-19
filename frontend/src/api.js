2a01c6847e890d28bdf6480e0302b46f7fd5181c
Bhargavi11122 <pappalabhargavi2@gmail.com>
2026-08-19 16:12:30 +0530

    update API base URL to live Render backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/',
+  baseURL: 'https://budget-buddy-v02g.onrender.com/api/',
 });
 
 // Automatically add JWT access token to headers
