eea55b4a69dcbcee4b69bf18f8f812fcf4a76d57
Neha Joshika <nehajoshika27@gmail.com>
2026-08-19 19:44:37 +0530

    Fix Render allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+g@kfmx@4t$4i9lqwh7mnhfqx1ub+vb)mfe@4w=am^9pm7e1zr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["budgetbuddy-a-full-stack-personal-budget-1hdo.onrender.com",]
 
 
 # Application definition
@@ -134,6 +134,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "https://YOUR-PROJECT.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
