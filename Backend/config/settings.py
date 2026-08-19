7a588a1b2dcd753342ed359204c0f5b56dec630a
Karuna-1512 <koppadikaruna1512@gmail.com>
2026-08-19 15:28:17 +0530

    Add allowed CORS origins for deployment

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -86,6 +86,8 @@ CORS_EXPOSE_HEADERS = ['Content-Type', 'Content-Disposition']
 ROOT_URLCONF = 'config.urls'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "https://budget-buddy-a-full-stack-personal.vercel.app",
+    "https://budgetbuddy-backend-h7j9.onrender.com"
     
 ]
 CORS_ALLOW_HEADERS = [
