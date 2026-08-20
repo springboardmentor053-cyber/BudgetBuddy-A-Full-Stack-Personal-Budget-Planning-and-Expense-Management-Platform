91a0204ced3e133e4843d301cdca1a027f5cb915
Bhargavi11122 <pappalabhargavi2@gmail.com>
2026-08-20 11:25:34 +0530

    switch email to SSL port 465

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -144,7 +144,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT =465
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'pappalabhargavi2@gmail.com'       # Your sender email
 EMAIL_HOST_PASSWORD = 'qgrfdiregsmqkegu'   # Gmail App Password (NOT personal password)
