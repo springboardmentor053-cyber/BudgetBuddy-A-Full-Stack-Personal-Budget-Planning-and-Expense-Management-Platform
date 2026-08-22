27e5a741ff3e7c9550f132b95bb1f9a3f608e42e
Anagha C <anaghacsreeragam@gmail.com>
2026-08-22 16:27:28 +0530

    Switch database from SQLite3 to PostgreSQL

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -100,13 +100,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
+import os
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        conn_max_age=600
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
